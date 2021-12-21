8919b10e9ee616fa58c21537ceac57e1203c7f0f
kk3939 <kyo51310@gmail.com>
2021-12-22 00:24:26 +0900

    style: chakra ui導入

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { Box, Center } from "@chakra-ui/react";
 
 const Index: React.VFC = () => {
-  return <h1>Index</h1>;
+  return (
+    <>
+      <Box w="100%" h="350px" bg="green.400">
+        <Center h="200px" fontSize="6xl" color="white">
+          Radio Programs
+        </Center>
+      </Box>
+    </>
+  );
 };
 
 export default Index;
