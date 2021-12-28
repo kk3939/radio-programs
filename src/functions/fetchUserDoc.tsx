9b427d9c78c7c1218c16fd5d7be4cfe7d93dacbe
kk3939 <kyo51310@gmail.com>
2021-12-29 01:26:27 +0900

    add: 新規登録と既存のユーザーに対するsignUp,login処理を分割して実装

--- src/functions/fetchUserDoc.tsx
+++ src/functions/fetchUserDoc.tsx
@@ -6,25 +6,11 @@ import {
   getDoc,
   QueryDocumentSnapshot,
 } from "firebase/firestore";
-import { db } from "../../firebase";
+import { converter, db } from "../../firebase";
 import { initialState } from "../redux/slice";
 import { UserDoc, UserProps } from "../types/global";
 
 export async function fetchUserDocs(id: string): Promise<UserProps> {
-  // fetchでデータを取ってくる際に、型を定義させてあげる。
-  const converter = () => ({
-    toFirestore: (data: UserDoc) => data,
-    fromFirestore: (snapshot: QueryDocumentSnapshot<DocumentData>): UserDoc => {
-      const data = snapshot.data();
-      return {
-        id: data.id,
-        name: data.name,
-        photoUrl: data.photoUrl,
-        radios: data.radios,
-      };
-    },
-  });
-
   const userDocRef: DocumentReference<UserDoc> = doc(
     db,
     "users",
