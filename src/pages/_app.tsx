7b1941e29e3c5ebbb5be0a8a91e28ac649ba9a1b
kk3939 <kyo51310@gmail.com>
2022-01-12 22:06:50 +0900

    add: progress bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from "next/app";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import "../asset/global.css";
 import { Provider } from "react-redux";
+import NextNprogress from "nextjs-progressbar";
 import { store } from "../redux/store";
 
 const theme = extendTheme({
@@ -19,6 +20,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
       <Provider store={store}>
+        <NextNprogress />
         <Component {...pageProps} />
       </Provider>
     </ChakraProvider>
