a5d49cea762de06baf61bb9231964100b976e75f
kk3939 <kyo51310@gmail.com>
2022-01-20 20:46:14 +0900

    add: a11yに関するaddonを追加

--- .storybook/main.js
+++ .storybook/main.js
@@ -9,7 +9,8 @@ module.exports = {
   ],
   "addons": [
     "@storybook/addon-links",
-    "@storybook/addon-essentials"
+    "@storybook/addon-essentials",
+    "@storybook/addon-a11y"
   ],
   "framework": "@storybook/react",
   webpackFinal: async (config) => {
